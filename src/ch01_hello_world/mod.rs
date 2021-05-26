32c249e3fb403d82de9a434079111ae5e4553744
Alex Black <wa7@sa34.cx>
2021-05-26 09:54:36 +0300

    Start

---
+++ src/ch01_hello_world/mod.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod ch01_hello_world;
+
+use ch01_hello_world as hello_world;
+
+fn main() {
+    hello_world::run();
+}
