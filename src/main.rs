4e9173e69332c3b1d77c4df8e402b9c43cadd0e6
Alex Black <wa7@sa34.cx>
2021-08-03 02:36:31 +0300

    ch09

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ mod ch05_types;
 mod ch08_3_fizzbuzz;
 mod ch08_5_1_3_pointers;
 mod ch09_2_1_capturing;
+mod ch09_2_2_as_input;
+mod ch09_2_6_2_sti;
 
 use ch01_0_hello_world as hello_world;
 use ch01_2_2_1_list as list;
@@ -19,6 +21,8 @@ use ch05_types as types;
 use ch08_3_fizzbuzz as fizzbuzz;
 use ch08_5_1_3_pointers as pointers;
 use ch09_2_1_capturing as capturing;
+use ch09_2_2_as_input as as_input;
+use ch09_2_6_2_sti as sti;
 
 fn main() {
     // hello_world::run();
@@ -29,5 +33,7 @@ fn main() {
     // types::run();
     // fizzbuzz::run();
     // pointers::run();
-    capturing::run();
+    // capturing::run();
+    // as_input::run();
+    sti::run();
 }
